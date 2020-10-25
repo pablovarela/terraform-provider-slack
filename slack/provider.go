ea2926cb377b821f27ebc28875e88320818345d4
Pablo Varela <contact@pablovarela.co.uk>
2020-10-25 16:26:54 +0000

    Move read conversation functions to conversations resource

--- slack/provider.go
+++ slack/provider.go
@@ -5,7 +5,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/slack-go/slack"
-	"sort"
 )
 
 // Provider returns a *schema.Provider
@@ -42,79 +41,6 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 	}
 	slackClient := slack.New(token.(string))
 	return slackClient, diags
-}
-
-func readChannelInfo(ctx context.Context, d *schema.ResourceData, client *slack.Client, id string) diag.Diagnostics {
-	channel, err := client.GetConversationInfoContext(ctx, id, false)
-	if err != nil {
-		return diag.Errorf("couldn't get conversation info for %s: %s", id, err)
-	}
-
-	users, _, err := client.GetUsersInConversationContext(ctx, &slack.GetUsersInConversationParameters{
-		ChannelID: channel.ID,
-	})
-	if err != nil {
-		return diag.Errorf("couldn't get users in conversation for %s: %s", channel.ID, err)
-	}
-	return updateChannelData(d, channel, users)
-}
-
-func updateChannelData(d *schema.ResourceData, channel *slack.Channel, users []string) diag.Diagnostics {
-	if channel.ID == "" {
-		return diag.Errorf("error setting id: returned channel does not have an id")
-	}
-	d.SetId(channel.ID)
-
-	if err := d.Set("name", channel.Name); err != nil {
-		return diag.Errorf("error setting name: %s", err)
-	}
-
-	if err := d.Set("topic", channel.Topic.Value); err != nil {
-		return diag.Errorf("error setting topic: %s", err)
-	}
-
-	if err := d.Set("purpose", channel.Purpose.Value); err != nil {
-		return diag.Errorf("error setting purpose: %s", err)
-	}
-
-	if err := d.Set("is_archived", channel.IsArchived); err != nil {
-		return diag.Errorf("error setting is_archived: %s", err)
-	}
-
-	if err := d.Set("is_shared", channel.IsShared); err != nil {
-		return diag.Errorf("error setting is_shared: %s", err)
-	}
-
-	if err := d.Set("is_ext_shared", channel.IsExtShared); err != nil {
-		return diag.Errorf("error setting is_ext_shared: %s", err)
-	}
-
-	if err := d.Set("is_org_shared", channel.IsOrgShared); err != nil {
-		return diag.Errorf("error setting is_org_shared: %s", err)
-	}
-
-	if err := d.Set("created", channel.Created); err != nil {
-		return diag.Errorf("error setting created: %s", err)
-	}
-
-	if err := d.Set("creator", channel.Creator); err != nil {
-		return diag.Errorf("error setting creator: %s", err)
-	}
-
-	if err := d.Set("is_private", channel.IsPrivate); err != nil {
-		return diag.Errorf("error setting is_private: %s", err)
-	}
-
-	if err := d.Set("is_general", channel.IsGeneral); err != nil {
-		return diag.Errorf("error setting is_general: %s", err)
-	}
-
-	sort.Strings(users)
-	if err := d.Set("members", users); err != nil {
-		return diag.Errorf("error setting members: %s", err)
-	}
-
-	return nil
 }
 
 func schemaSetToSlice(set *schema.Set) []string {
--- slack/resource_conversation.go
+++ slack/resource_conversation.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/slack-go/slack"
+	"sort"
 )
 
 func resourceSlackConversation() *schema.Resource {
@@ -198,15 +199,6 @@ func resourceSlackConversationUpdate(ctx context.Context, d *schema.ResourceData
 	return resourceSlackConversationRead(ctx, d, m)
 }
 
-func archiveConversationWithContext(ctx context.Context, client *slack.Client, id string) error {
-	if err := client.ArchiveConversationContext(ctx, id); err != nil {
-		if err.Error() != "already_archived" {
-			return fmt.Errorf("couldn't archive conversation %s: %s", id, err)
-		}
-	}
-	return nil
-}
-
 func resourceSlackConversationDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	client := m.(*slack.Client)
@@ -218,4 +210,86 @@ func resourceSlackConversationDelete(ctx context.Context, d *schema.ResourceData
 	}
 
 	return diags
+}
+
+func readChannelInfo(ctx context.Context, d *schema.ResourceData, client *slack.Client, id string) diag.Diagnostics {
+	channel, err := client.GetConversationInfoContext(ctx, id, false)
+	if err != nil {
+		return diag.Errorf("couldn't get conversation info for %s: %s", id, err)
+	}
+
+	users, _, err := client.GetUsersInConversationContext(ctx, &slack.GetUsersInConversationParameters{
+		ChannelID: channel.ID,
+	})
+	if err != nil {
+		return diag.Errorf("couldn't get users in conversation for %s: %s", channel.ID, err)
+	}
+	return updateChannelData(d, channel, users)
+}
+
+func updateChannelData(d *schema.ResourceData, channel *slack.Channel, users []string) diag.Diagnostics {
+	if channel.ID == "" {
+		return diag.Errorf("error setting id: returned channel does not have an id")
+	}
+	d.SetId(channel.ID)
+
+	if err := d.Set("name", channel.Name); err != nil {
+		return diag.Errorf("error setting name: %s", err)
+	}
+
+	if err := d.Set("topic", channel.Topic.Value); err != nil {
+		return diag.Errorf("error setting topic: %s", err)
+	}
+
+	if err := d.Set("purpose", channel.Purpose.Value); err != nil {
+		return diag.Errorf("error setting purpose: %s", err)
+	}
+
+	if err := d.Set("is_archived", channel.IsArchived); err != nil {
+		return diag.Errorf("error setting is_archived: %s", err)
+	}
+
+	if err := d.Set("is_shared", channel.IsShared); err != nil {
+		return diag.Errorf("error setting is_shared: %s", err)
+	}
+
+	if err := d.Set("is_ext_shared", channel.IsExtShared); err != nil {
+		return diag.Errorf("error setting is_ext_shared: %s", err)
+	}
+
+	if err := d.Set("is_org_shared", channel.IsOrgShared); err != nil {
+		return diag.Errorf("error setting is_org_shared: %s", err)
+	}
+
+	if err := d.Set("created", channel.Created); err != nil {
+		return diag.Errorf("error setting created: %s", err)
+	}
+
+	if err := d.Set("creator", channel.Creator); err != nil {
+		return diag.Errorf("error setting creator: %s", err)
+	}
+
+	if err := d.Set("is_private", channel.IsPrivate); err != nil {
+		return diag.Errorf("error setting is_private: %s", err)
+	}
+
+	if err := d.Set("is_general", channel.IsGeneral); err != nil {
+		return diag.Errorf("error setting is_general: %s", err)
+	}
+
+	sort.Strings(users)
+	if err := d.Set("members", users); err != nil {
+		return diag.Errorf("error setting members: %s", err)
+	}
+
+	return nil
+}
+
+func archiveConversationWithContext(ctx context.Context, client *slack.Client, id string) error {
+	if err := client.ArchiveConversationContext(ctx, id); err != nil {
+		if err.Error() != "already_archived" {
+			return fmt.Errorf("couldn't archive conversation %s: %s", id, err)
+		}
+	}
+	return nil
 }
